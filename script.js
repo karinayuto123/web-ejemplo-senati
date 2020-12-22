7e18b9e8556049467c2b0ea8bf18f6db4e31accc
karinayuto123 <76476877+karinayuto123@users.noreply.github.com>
2020-12-22 06:16:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let header = document.querySelectorAll(".header");
+
+for(let i = 0; i < header.length; i++){
+	header[i].addEventListener("click", function(){
+		let ddd = this.nextElementSibling;
+		ddd.classList.toggle("active");
+
+		if(this.firstElementChild.innerText === '+'){
+			this.firstElementChild.innerText = "+"
+		}else{
+			this.firstElementChild.innerText = "+"
+		}
+	})
+}
